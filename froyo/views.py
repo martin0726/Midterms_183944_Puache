18a5c1a489583b163e52fdbe0b824067b78da389
martin0726 <60959558+martin0726@users.noreply.github.com>
2020-03-20 21:03:58 +0800

    Created and tested views for ingredients model

--- froyo/views.py
+++ froyo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
+
+
+class IngredientsListView(generic.ListView):
+	template_name = 'ingredients_list.html'
+
+
+class IngredientsDetailView(generic.DetailView):
+	template_name = 'ingredients_detail.html'
+
+
+class IngredientsUpdateView(generic.Updateview):
+	template_name = 'ingredients_update_form.html'
+
+
+class IngredientsCreateView(generic.CrateView):
+	template_name = 'ingredients_create_form.html'
+
 
 # Create your views here.
